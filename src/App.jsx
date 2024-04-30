9c8bd1df34550e0b36fc9a92e0980fb7fbe4c004
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-30 13:38:12 -0400

    Create page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+// App Layout Component
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div id="app-main-content-container">
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 function App() {
-  return <></>;
+  // Router
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "posts/:postId", element: <Post /> },
+        { path: "users/login", element: <Login /> },
+        { path: "users/signup", element: <Signup /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <a href="/">
+          <h1>blog client</h1>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
