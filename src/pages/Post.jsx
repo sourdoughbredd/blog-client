660b24ca5e615c8d6c6d67172acfbf789d356ac2
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-01 15:26:02 -0400

    Finish basic Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,6 +2,72 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { fetchPost, fetchComments } from "../api";
 
+// Utils
+function formatDate(timestamp) {
+  return new Date(timestamp).toLocaleString("en-US", {
+    // weekday: "long", // "Monday"
+    year: "numeric", // "2024"
+    month: "numeric", // "April"
+    day: "numeric", // "22"
+    hour: "numeric", // "12"
+    minute: "numeric", // "54"
+    // timeZoneName: "short", // "GMT"
+  });
+}
+
+// Components
+const PostContent = ({ post }) => {
+  return (
+    <div className="post-content-container">
+      <h1>{post.title}</h1>
+      <h3>
+        by {post.user.username} on {formatDate(post.timestamp)}
+      </h3>
+      <p>{post.text}</p>
+    </div>
+  );
+};
+
+const CommentForm = () => {
+  return (
+    <div className="comment-form-container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="text">Add your comment</label>
+          <textarea
+            name="text"
+            cols="30"
+            rows="3"
+            placeholder="Your comment here..."
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+const Comments = ({ comments }) => {
+  return (
+    <div className="comments-container">
+      <h2>Comments</h2>
+      <CommentForm />
+      {comments.map((comment) => (
+        <>
+          <hr />
+          <div className="comment-card">
+            <p>{comment.text}</p>
+            <p>
+              by {comment.user.username} on {formatDate(comment.timestamp)}{" "}
+            </p>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+// Page
 const Post = () => {
   const { postId } = useParams();
 
@@ -63,17 +129,8 @@ const Post = () => {
 
   return (
     <>
-      <h1>{post.title}</h1>
-      <h3>by {post.user.username}</h3>
-      <p>{post.text}</p>
-      <h2>Comments</h2>
-      <ul>
-        {commentsLoading ? (
-          <li>Loading comments...</li>
-        ) : (
-          comments.map((comment) => <li key={comment._id}>{comment.text}</li>)
-        )}
-      </ul>
+      <PostContent post={post} />
+      <Comments comments={comments} />
     </>
   );
 };
